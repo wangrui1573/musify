fcc458cbd1e43d4830769cdd11608719fd08da3e
wangrui1573 <real-wang@qq.com>
2022-05-15 19:02:06 +0800

    Update LibrarySong.js

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -37,7 +37,12 @@ const LibrarySong = ({
       onClick={songSelectHandler}
       className={`library-song ${song.active ? "selected" : ""}`}
     >
+      
+      /*
       <img src={song.cover} alt={song.name} />
+
+      */
+
       <div className="song-description">
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
