acf61ef3ec06e13163d388a9dbfcac7c48f1388a
瑞哥 <42831156+wangrui1573@users.noreply.github.com>
2022-05-16 16:06:44 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ import skechers from "./audio/Skechers.mp3";
 
 
 // 以 "dialog-" 开头,以".vue"结尾
-const allComponents = require.context('./audio', false, /^dialog.*\.flac/)
+const allComponents = require.context('./audio', false, /\.flac/))
 let res_components = {}
 allComponents.keys().forEach(fileName => {
   let comp = allComponents(fileName)
