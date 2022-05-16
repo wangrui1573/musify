1265d7ccaa846e3aa109d1c08c9a924bb635d066
瑞哥 <42831156+wangrui1573@users.noreply.github.com>
2022-05-17 00:51:35 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,9 +7,8 @@ import stuckWithU from "./audio/Stuck_With_U.mp3";
 import cradles from "./audio/Cradles.mp3";
 import skechers from "./audio/Skechers.mp3";
 import 爱丫爱丫 from "./audio/爱丫爱丫.flac";
-import test from "http://ys-d.ysepan.com/604688930/318647726/i571G66437LPJIUIjHt9d4/%E7%A7%91%E6%8A%80%E5%A4%A7%E6%B0%94Z.mp3";
 
-const s = document.createElement('fs');
+const s = document.createElement('audio');
 s.type = 'audio';
   s.src = 'http://ys-d.ysepan.com/604688930/318647726/i571G66437LPJIUIjHt9d4/%E7%A7%91%E6%8A%80%E5%A4%A7%E6%B0%94Z.mp3';
   document.body.appendChild(s);
